8cc92b01d784a54096a3bd4e596acc2ce3b40b24
Bun Pape <benjaminhpage@me.com>
2021-10-25 18:48:01 -0400

    added fight results

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,38 @@
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
+console.log(playerName, playerAttack, playerHealth)
 
-var name = "your name";
-console.log(name);
+var enemyName = "Evil Sbeve";
+var enemyHealth = 50;
+var enemyAttack = 12;
+console.log(enemyName, enemyAttack, enemyHealth)
 
-function fight (){
-    window.alert("The fight has begun!");
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+    
+    if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+    }
+    else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    playerHealth = playerHealth - enemyAttack;
+    console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+    } 
+    else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 }
 
-// fight ();
-
+fight();
