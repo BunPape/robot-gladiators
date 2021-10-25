10812772371771c76e3656fab9ecb9f20594d16e
Bun Pape <benjaminhpage@me.com>
2021-10-25 18:24:56 -0400

    created HTML, and initial JS features

---
+++ assets/js/game.js
@@ -0,0 +1,12 @@
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+var name = "your name";
+console.log(name);
+
+function fight (){
+    window.alert("The fight has begun!");
+}
+
+// fight ();
+
